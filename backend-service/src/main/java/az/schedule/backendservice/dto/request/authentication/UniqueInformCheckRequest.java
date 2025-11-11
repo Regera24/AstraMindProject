f6b1e91e614445f97ff3e8d25299083a6f7cd7f6
Regera24 <regera241@gmail.com>
2025-11-12 01:28:36 +0700

    Add: Add setter

--- backend-service/src/main/java/az/schedule/backendservice/dto/request/authentication/UniqueInformCheckRequest.java
+++ backend-service/src/main/java/az/schedule/backendservice/dto/request/authentication/UniqueInformCheckRequest.java
@@ -6,9 +6,11 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UniqueInformCheckRequest {
